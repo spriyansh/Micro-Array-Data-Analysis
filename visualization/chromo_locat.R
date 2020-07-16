a5d1b7434ce1d110bb189803045df2e99e6facc1
PRIYANSH SRIVASTAVA <34842808+spriyansh@users.noreply.github.com>
2020-07-16 10:02:44 +0530

    Delete chromo_locat.R

--- visualization/chromo_locat.R
+++
@@ -1,7 +0,0 @@
-# Loading library 
-library(chromoMap)
-
-# Plotting
-chromoMap("Chrom_info.txt","MS.txt",
-          labels=T, canvas_width = 1500, canvas_height = 1000,
-          chr_color = c("Black","blue"), chr_width = 20, chr_length = 5, ch_gap = 15)
